9f71996e71db35df575b769025a4e22b2429d03c
unknown <jefferson.santos@jefferson-1382.Alternate.lco>
2016-10-21 15:12:14 -0200

    Created project converter integer to hexadecimal

---
+++ IntToHex/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace IntToHex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(39, 27);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(222, 20);
+            this.txtDecimal.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(267, 25);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(146, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert!";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.onClickConvert);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(39, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(374, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 315);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtDecimal);
+            this.Name = "Form1";
+            this.Text = "Convert Decimal to Hex";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ IntToHex/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntToHex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Six decimal places
+        private const int C_FIX = 6;  
+
+        private void onClickConvert(object sender, EventArgs e)
+        {
+
+            label1.Text = "";
+            listBox1.BackColor = Color.White;
+
+            if (!string.IsNullOrEmpty(txtDecimal.Text))
+            {
+                string a = string.Format("#{0}", IntToHexString(int.Parse(txtDecimal.Text), C_FIX));
+                label1.Text = a;
+                listBox1.BackColor = ColorTranslator.FromHtml(a);
+            }
+            else
+            {
+                MessageBox.Show("Field can not be blank");                
+            }
+            
+        }
+
+        private static String IntToHexString(int n, int len)
+        {
+            char[] ch = new char[len--];
+            for (int i = len; i >= 0; i--)
+            {
+                ch[len - i] = ByteToHexChar((byte)((uint)(n >> 4 * i) & 15));
+            }
+            return new String(ch);
+        }
+
+        private static char ByteToHexChar(byte b)
+        {
+            if (b < 0 || b > 15)
+                throw new Exception("IntToHexChar: input out of range for Hex value");
+            return b < 10 ? (char)(b + 48) : (char)(b + 55);
+        }
+
+    }
+}
